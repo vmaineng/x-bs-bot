8738ed8b389171491b1e6244a9bef4cddb730bc0
Mai Vang <vmaineng@gmail.com>
2025-01-19 18:23:56 -0800

    added in 400

--- main.py
+++ main.py
@@ -9,11 +9,24 @@ X_CONSUMER_SECRET = os.getenv("X_CONSUMER_SECRET")
 X_ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
 X_ACCESS_SECRET = os.getenv("X_ACCESS_SECRET")
 
-x_client = tweepy.Client(
+# x_client = tweepy.Client(
+#     consumer_key=X_CONSUMER_KEY,
+#     consumer_secret=X_CONSUMER_SECRET,
+#     access_token=X_ACCESS_TOKEN,
+#     access_token_secret=X_ACCESS_SECRET
+# )
+# response = x_client.create_tweet(text='Project posting')
+# print(response)
+
+auth = tweepy.OAuth1UserHandler(
     consumer_key=X_CONSUMER_KEY,
     consumer_secret=X_CONSUMER_SECRET,
     access_token=X_ACCESS_TOKEN,
-    access_token_secret=X_ACCESS_SECRET,
+    access_token_secret=X_ACCESS_SECRET
 )
-response = x_client.create_tweet(text='Project posting')
-print(response)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
