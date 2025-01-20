1c72a1096d5200b52563b968a3cd2feb24bf0808
Mai Vang <vmaineng@gmail.com>
2025-01-19 18:20:48 -0800

    added in py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+from dotenv import load_dotenv
 import tweepy
+
+load_dotenv()
+
+X_CONSUMER_KEY = os.getenv("X_CONSUMER_KEY")
+X_CONSUMER_SECRET = os.getenv("X_CONSUMER_SECRET")
+X_ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
+X_ACCESS_SECRET = os.getenv("X_ACCESS_SECRET")
+
+x_client = tweepy.Client(
+    consumer_key=X_CONSUMER_KEY,
+    consumer_secret=X_CONSUMER_SECRET,
+    access_token=X_ACCESS_TOKEN,
+    access_token_secret=X_ACCESS_SECRET,
+)
+response = x_client.create_tweet(text='Project posting')
+print(response)
