d8dd921518269a19f9f68635b8d2bd09450c41e2
Mai Vang <vmaineng@gmail.com>
2025-01-21 12:39:27 -0800

    added in pw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from dotenv import load_dotenv 
 import os
-from dotenv import load_dotenv
 import tweepy
+from atproto import Client, client_utils
 
 load_dotenv()
 
@@ -9,24 +10,33 @@ X_CONSUMER_SECRET = os.getenv("X_CONSUMER_SECRET")
 X_ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
 X_ACCESS_SECRET = os.getenv("X_ACCESS_SECRET")
 
-# x_client = tweepy.Client(
-#     consumer_key=X_CONSUMER_KEY,
-#     consumer_secret=X_CONSUMER_SECRET,
-#     access_token=X_ACCESS_TOKEN,
-#     access_token_secret=X_ACCESS_SECRET
-# )
-# response = x_client.create_tweet(text='Project posting')
-# print(response)
+BLUESKY_USER = os.getenv("BLUESKY_USER")
+BLUESKY_PASSWORD = os.getenv("BLUESKY_PASSWORD")
 
-auth = tweepy.OAuth1UserHandler(
+x_client = tweepy.Client(
     consumer_key=X_CONSUMER_KEY,
     consumer_secret=X_CONSUMER_SECRET,
     access_token=X_ACCESS_TOKEN,
     access_token_secret=X_ACCESS_SECRET
 )
+response = x_client.create_tweet(text='Project posting')
+print(response)
 
-api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+
+
+
+
+# auth = tweepy.OAuth1UserHandler(
+#     consumer_key=X_CONSUMER_KEY,
+#     consumer_secret=X_CONSUMER_SECRET,
+#     access_token=X_ACCESS_TOKEN,
+#     access_token_secret=X_ACCESS_SECRET,
+# )
+
+# api = tweepy.API(auth)
+
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
